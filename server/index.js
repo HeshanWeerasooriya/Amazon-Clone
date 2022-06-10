e73ea7b46d9daa46e31af4cc12f191dcfbaffbd8
Heshan <heshan.imedha@gmail.com>
2022-06-10 22:38:05 +0530

    Connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // IMPORT FROM PACKAGES
 const express = require("express");
+const mongoose = require("mongoose");
 
 //IMPORT FROM OTHER FILES
 const authRouter = require('./routes/auth')
@@ -7,11 +8,19 @@ const authRouter = require('./routes/auth')
 //INIT
 const PORT = 3000;
 const app = express();
+const DB = "mongodb+srv://Heshan:8HitDJonCvRc9Yjf@cluster0.syxew.mongodb.net/?retryWrites=true&w=majority";
 
 //MIDDLEWARE
 app.use(authRouter);
 
+//CONNECTIONS
+mongoose.connect().then(() =>{
+    console.log("Connection Successfull");
+})
+.catch((e) => {
+    console.log(e);
+});
+
 app.listen(PORT, () => {
     console.log('connected at port ' + PORT);
 });
-
