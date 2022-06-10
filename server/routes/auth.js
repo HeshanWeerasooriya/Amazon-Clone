923d09747549ee4cca9cd35d2573ac1eae18f19f
Heshan <heshan.imedha@gmail.com>
2022-06-10 22:06:18 +0530

    Sign up Route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const authRouter = express.Router();
 
-authRouter.get("/user", (req, res) => {
-    res.json({msg: "heshan"});
+authRouter.post("/api/signup", (req, res) => {
+    // get the data from client
+   const {name, email, password} = req.body;
+   
+   // post that data in database
+   // return that data to the user
 });
 
 module.exports = authRouter;
