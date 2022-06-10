4a8675d23b0ce461434e4c1d109923d926028554
Heshan <heshan.imedha@gmail.com>
2022-06-10 21:35:19 +0530

    Middleware

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const express = require("express");
 
 //IMPORT FROM OTHER FILES
 const authRouter = require('./routes/auth')
+
 //INIT
 const PORT = 3000;
 const app = express();
+
+//MIDDLEWARE
+app.use(authRouter);
 
 app.listen(PORT, () => {
     console.log('connected at port ' + PORT);
