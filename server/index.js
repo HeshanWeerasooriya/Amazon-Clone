aeecb9bd8f63ee0e349b001f9beb0c970f4a3f36
Heshan <heshan.imedha@gmail.com>
2022-06-10 17:44:50 +0530

    Creating Server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+console.log("Hello World!")
+
+// Import express
+const express = require("express");
+
+const PORT = 3000;
+
+const app = express();
+
+//CREATING AN API
+app.listen(PORT, "0.0.0.0", () => {
+    console.log('connected at port ' + PORT);
+});
+
