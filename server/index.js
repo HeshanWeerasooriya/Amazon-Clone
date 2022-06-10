710c82b62a7742d38f5cb6362d7ee98a7efb81f0
Heshan <heshan.imedha@gmail.com>
2022-06-10 18:06:47 +0530

    Create GET API

--- server/index.js
+++ server/index.js
@@ -8,7 +8,19 @@ const PORT = 3000;
 const app = express();
 
 //CREATING AN API
-app.listen(PORT, "0.0.0.0", () => {
+
+//http://localhost:3000/hello-world
+app.get("/hello-world", (req, res) => {
+    res.send( "hello world!");
+});
+
+//http://localhost:3000/hello-world-json
+app.get("/hello-world-json", (req, res) => {
+    res.json({ hi: "hello world!"});
+   
+});
+
+app.listen(PORT, () => {
     console.log('connected at port ' + PORT);
 });
 
